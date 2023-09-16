287ff2a41f3628c9a7303aef3d72d30b73a50ed4
Abdi Urgessa <abdiurgessa9@gmail.com>
2023-09-16 16:14:20 +0300

    finalized

---
+++ README.md
@@ -0,0 +1,7 @@
+## Inventory Management System
+
+<!-- About the Project -->
+
+### About the Project
+
+is System which provides powerful low-level stock control and part tracking. The core of this system is a Python/Django database backend which provides an admin interface (web-based) and a REST API for interaction with external interfaces and applications. A powerful plugin system provides support for custom applications and extensions.
---
+++ src/frontend/src/components/tables/stock/StockItemTable.tsx
@@ -0,0 +1,141 @@
+import { t } from '@lingui/macro';
+import { Group } from '@mantine/core';
+import { IconEdit, IconTrash } from '@tabler/icons-react';
+import { useEffect, useMemo, useState } from 'react';
+
+import { notYetImplemented } from '../../../functions/notifications';
+import { ActionButton } from '../../items/ActionButton';
+import { ThumbnailHoverCard } from '../../items/Thumbnail';
+import { TableColumn } from '../Column';
+import { TableFilter } from '../Filter';
+import { RowAction } from '../RowActions';
+import { InvenTreeTable } from './../InvenTreeTable';
+
+/**
+ * Construct a list of columns for the stock item table
+ */
+function stockItemTableColumns(): TableColumn[] {
+  return [
+    {
+      accessor: 'part',
+      sortable: true,
+      title: t`Part`,
+      render: function (record: any) {
+        let part = record.part_detail;
+        return (
+          <ThumbnailHoverCard
+            src={part.thumbnail || part.image}
+            text={part.name}
+            link=""
+          />
+        );
+      }
+    },
+    {
+      accessor: 'part_detail.description',
+      sortable: false,
+      switchable: true,
+      title: t`Description`
+    },
+    {
+      accessor: 'quantity',
+      sortable: true,
+      title: t`Stock`
+      // TODO: Custom renderer for stock quantity
+    },
+    {
+      accessor: 'status',
+      sortable: true,
+      switchable: true,
+      filter: true,
+      title: t`Status`
+      // TODO: Custom renderer for stock status label
+    },
+    {
+      accessor: 'batch',
+      sortable: true,
+      switchable: true,
+      title: t`Batch`
+    },
+    {
+      accessor: 'location',
+      sortable: true,
+      switchable: true,
+      title: t`Location`,
+      render: function (record: any) {
+        // TODO: Custom renderer for location
+        return record.location;
+      }
+    }
+    // TODO: stocktake column
+    // TODO: expiry date
+    // TODO: last updated
+    // TODO: purchase order
+    // TODO: Supplier part
+    // TODO: purchase price
+    // TODO: stock value
+    // TODO: packaging
+    // TODO: notes
+  ];
+}
+
+/**
+ * Construct a list of available filters for the stock item table
+ */
+function stockItemTableFilters(): TableFilter[] {
+  return [
+    {
+      name: 'test_filter',
+      label: t`Test Filter`,
+      description: t`This is a test filter`,
+      type: 'choice',
+      choiceFunction: () => [
+        { value: '1', label: 'One' },
+        { value: '2', label: 'Two' },
+        { value: '3', label: 'Three' }
+      ]
+    }
+  ];
+}
+
+/*
+ * Load a table of stock items
+ */
+export function StockItemTable({ params = {} }: { params?: any }) {
+  let tableParams = useMemo(() => {
+    return {
+      part_detail: true,
+      location_detail: true,
+      ...params
+    };
+  }, [params]);
+
+  let tableColumns = useMemo(() => stockItemTableColumns(), []);
+  let tableFilters = useMemo(() => stockItemTableFilters(), []);
+
+  function stockItemRowActions(record: any): RowAction[] {
+    let actions: RowAction[] = [];
+
+    actions.push({
+      title: t`Edit`,
+      onClick: () => {
+        notYetImplemented();
+      }
+    });
+
+    return actions;
+  }
+
+  return (
+    <InvenTreeTable
+      url="stock/"
+      tableKey="stock-table"
+      enableDownload
+      enableSelection
+      params={tableParams}
+      columns={tableColumns}
+      customFilters={tableFilters}
+      rowActions={stockItemRowActions}
+    />
+  );
+}
